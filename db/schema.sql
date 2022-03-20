88fd24c118deddeabd7374ec8149630419851a0a
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-20 09:46:02 -0400

    schema added

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     first_name VARCHAR(30),
+     last_name VARCHAR(30),
+     role_id INT,
+     manager_id INT NULL,
+     FOREIGN KEY (role_id)
+     REFERENCES role(id)
+     ON DELETE SET NULL
+);
